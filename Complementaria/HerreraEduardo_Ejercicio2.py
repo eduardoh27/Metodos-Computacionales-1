10575a8410696c11735333ccb012d09fe377c1b5
eduardoh27 <ej.herreraa@uniandes.edu.co>
2021-08-22 21:31:25 -0500

    Semana 2

---
+++ Complementaria/HerreraEduardo_Ejercicio2.py
@@ -0,0 +1,13 @@
+import math 
+
+def angulo_ideal(h: float, v0: float)-> float:
+    g = 9.8
+    theta = to_degrees(math.atan(v0/(v0**2+2*g*h)**0.5))
+    return round(theta, 1)
+
+def to_degrees(a):
+    return a/math.pi*180
+
+altura = float(input("Ingrese la altura: "))
+velocidad = float(input("Ingrese la velocidad: "))
+print("El ángulo para lograr la distancia máxima es "+str(angulo_ideal(altura, velocidad))+"°")
