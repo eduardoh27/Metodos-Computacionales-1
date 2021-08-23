f9eec211d219d3e6b61633c9fba306a45a98a0b2
eduardoh27 <ej.herreraa@uniandes.edu.co>
2021-08-22 21:34:00 -0500

    Sem 2

--- Complementaria/HerreraEduardo_Ejercicio2.py
+++ Complementaria/HerreraEduardo_Ejercicio2.py
@@ -5,8 +5,8 @@ def angulo_ideal(h: float, v0: float)-> float:
     theta = to_degrees(math.atan(v0/(v0**2+2*g*h)**0.5))
     return round(theta, 1)
 
-def to_degrees(a):
-    return a/math.pi*180
+def to_degrees(theta: float)-> float:
+    return theta/math.pi*180
 
 altura = float(input("Ingrese la altura: "))
 velocidad = float(input("Ingrese la velocidad: "))
