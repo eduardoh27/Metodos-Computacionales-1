36ef1fd8168bbcc0a980cae2f3d38ab5003357d8
eduardoh27 <ej.herreraa@uniandes.edu.co>
2021-09-01 22:36:11 -0500

    .

--- Magistral/Semana4/Semana4_EduardoHerrera_JuanIdarraga.py
+++ Magistral/Semana4/Semana4_EduardoHerrera_JuanIdarraga.py
@@ -2,21 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
 
+
+"""
+Método 1
+"""
+
 def Biseccion(a,b,f,n,x1, y1, n1,tolerance=1e-10):
-    
+
     i = 0
     root = 1e6
     error = 0.
 
     while i < n:
-        
+
         m1 = (a + b)/2.
-        
-        
+
+
         if f(m1,x1,y1,n1) == 0.: # Encontre la raiz
             root = m1
             break
-            
+
         if f(m1,x1,y1,n1)*f(b,x1,y1,n1) < 0:
             a = m1
         elif f(a,x1,y1,n1)*f(m1,x1,y1,n1) < 0:
@@ -24,16 +29,15 @@ def Biseccion(a,b,f,n,x1, y1, n1,tolerance=1e-10):
         else:
             root = 'No-roots'
             break
-            
+
         error = np.abs(root - m1)
         root = m1
-        
+
         if error <= tolerance:
             break
-        
-        
+
         i += 1
-    
+
     return root, error
 
 
@@ -58,41 +62,54 @@ def Lagrange(x,xi,i,n):
 
     return prod
 
-
 x1 = [1.4000, 3.5000, 5.6000]
 y1 = [0.4007, 0.5941, 0.2980]
 n = 2
 
-"""
-n = 2
-x = np.linspace(0, 8, 10000)
-px2 = Poly(3, x1, y1, 2)
-#print(px2)
-"""
-# Usamos el método de bisección desde 0.1 para hallar la raíz. 
+# Usamos el método de bisección desde 0.1 para hallar la raíz.
 # La raíz es el punto de mayor alcance
+
 x_max, _ = Biseccion(0.1,10, Poly, 100, x1, y1, n)
 
 
-plt.scatter(x1, y1, color='r')
-plt.plot(x, px2)
+x = np.linspace(0,x_max,30)
+px2 = Poly(x,x1,y1,2)
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.plot(x,px2, c="gold")
+ax.set_title(r'Movimiento de una bala',fontsize=16)
+ax.set_xlabel(r'x[m]',fontsize=14)
+ax.set_ylabel(r'y[m]',fontsize=14)
 plt.show()
 
 
 
-def Linearlagrange2(a,b,c,fa,fb,fc):
+"""
+Método 2
+"""
+
+# Término lineal
+def LinearLagrange1(a,b,c,fa,fb,fc):
     return -(b+c)*fa/((a-b)*(a-c)) -(a+c)*fb/((b-a)*(b-c)) -(a+b)*fc/((c-a)*(c-b))
+
+# Término cuadrático
+def LinearLagrange2(a,b,c,fa,fb,fc):
+    return (fa/((a-b)*(a-c))) + (fb/((b-a)*(b-c))) + (fc/((c-a)*(c-b)))
 
 # x1 = [1.4000, 3.5000, 5.6000]
 # y1 = [0.4007, 0.5941, 0.2980]
 
 g = 9.8
+tanangle = LinearLagrange1(x1[0], x1[1], x1[2],  y1[0], y1[1], y1[2])
+angle = np.arctan(tanangle)
 
-# x = np.linspace(0,10,10)
-tanangle = Linearlagrange2(1.4000, 3.5000, 5.6000,  0.4007, 0.5941, 0.2980)
-angle = round(np.degrees(np.arctan(tanangle)),2)
-print(f"El ángulo inicial del disparo fue {angle}")
+expr_cuad = LinearLagrange2(x1[0], x1[1], x1[2],  y1[0], y1[1], y1[2])
+v0 = np.sqrt((-g)/((np.cos(angle)**2)*2*expr_cuad))
 
+# Con el método 1:
+# De la distancia horizontal máxima x_max = (v0^2 sin(2x))/g
+# v_0 = np.sqrt(x_max*g/np.sin(2*angle))
+# Y se obtiene el mismo resultado
 
-v_0 = round(np.sqrt(x_max*g/np.sin(2*angle)),2)
-print(f"La velocidad inicial del disparo fue {v_0}")
+print(f"El ángulo inicial del disparo fue {round(np.degrees(angle),2)}")
+print(f"La velocidad inicial del disparo fue {round(v0,2)}")
