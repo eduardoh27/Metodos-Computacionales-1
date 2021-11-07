b8d05dcf36236f034a7d853f3cac83da09d6a5ce
eduardoh27 <78046940+eduardoh27@users.noreply.github.com>
2021-11-07 17:50:57 -0500

    Fin semana 12 comple

--- Complementaria/Semana12/semana12test.py
+++ Complementaria/Semana12/semana12test.py
@@ -4,32 +4,23 @@ import sympy as sym
 from sympy import exp
 
 dataList = [130,122,119,142,136,127,120,152,141,132,127,118,150,141,133,137,129,142]
-print(min(dataList), max(dataList))
+
 media, desv = norm.fit(dataList)
-#media = np.mean(dataList)
-#desv = np.std(dataList)
-print(media, desv)
+print(f'media, desv = {media, desv}')
 
-f = lambda x, sigma: (1/(2*sigma**2))*np.exp(-np.abs(x)/(sigma**2))
 
-probaTotal = 0
-for dato in dataList:
-    probaTotal += f(dato, desv)
+# 1.
 
-print(probaTotal)
-lista = f(media, desv)
-
-print(lista)
-
-print(lista.sum())
-
-sigma = desv
-#sigma = 1221
+sigma = np.random.randint(10) # no importa el valor de sigma
 
 x = sym.Symbol('x', Real=True)
 #y = sym.Symbol('y', Real=True)
 pdf = (1/(2*sigma**2))*exp(-np.abs(x)/(sigma**2))
 
 integral = sym.integrate(pdf, (x, -sym.oo, sym.oo))
-
 print(integral)
+
+
+# 2.
+
+print(f'sigma = {np.sqrt(media)}')
