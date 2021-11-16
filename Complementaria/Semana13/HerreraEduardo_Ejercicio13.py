0bdea2378f0359fc1744e766f800f8491b6196da
eduardoh27 <78046940+eduardoh27@users.noreply.github.com>
2021-11-16 17:44:31 -0500

    fin sem 13

--- Complementaria/Semana13/HerreraEduardo_Ejercicio13.py
+++ Complementaria/Semana13/HerreraEduardo_Ejercicio13.py
@@ -1,15 +1,22 @@
-import numpy
+import numpy as np 
+from scipy.stats import norm
 
 def main():
     
     medicionesA = [6.24, 6.31, 6.28, 6.30, 6.25, 6.26, 6.24, 6.29, 6.22, 6.28]
     medicionesB = [6.27, 6.25, 6.33, 6.27, 6.24, 6.31, 6.28, 6.29, 6.34, 6.27]
-    
-    p_value = 0
+    sigma = 0.05
+    n_sample = len(medicionesA)
+    miu_sampleA = sum(medicionesA)/n_sample
+    miu_sampleB = sum(medicionesB)/n_sample
+    EP = (miu_sampleA-miu_sampleB)/np.sqrt(sigma**2/n_sample + sigma**2/n_sample)
+    p_value = norm.cdf(EP)*2
     alpha = 0.05
-    conclusion = 'conclusion'
-    
+    conclusion = ("dado que el p_value es mayor que el nivel de significancia"
+                    " de 0.05, las soluciones vienen de la misma fuente.")
     print(f"p value = {p_value}, la conclusion es: {conclusion}")
+    
 
 if __name__ == '__main__':
     main()
+    
